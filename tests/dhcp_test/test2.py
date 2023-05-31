e06511b6f0fe2382ec7a1f09c2d0f0a72754d726
jayfeather233 <jayfeather233@gmail.com>
2023-06-01 05:08:26 +0800

    update dhcp

--- tests/dhcp_test/test2.py
+++ tests/dhcp_test/test2.py
@@ -34,17 +34,13 @@ class TestTopo(Topo):
         h2 = self.addHost('h2', ip='no ip defined/8')
         h3 = self.addHost('h3', ip='no ip defined/8')
         s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
         self.addLink(h1, s1)
         self.addLink(h2, s1)
-        self.addLink(s1, s2)
-        self.addLink(h3, s2)
+        self.addLink(h3, s1)
 
 #    h1    h2
 #      \  /
 #       s1
-#       |
-#       s2
 #       |
 #       h3
 #
